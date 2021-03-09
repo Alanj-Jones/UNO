b9749de5f6fa0c3b164271e3a6045fd081d98d1c
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-03-09 20:54:10 -0300

    Player Round Iterator implemented

--- src/main/java/player/PlayerRoundIterator.java
+++ src/main/java/player/PlayerRoundIterator.java
@@ -1,34 +1,56 @@
 package src.main.java.player;
 
+import java.util.Arrays;
 import java.util.UUID;
 import java.util.stream.Stream;
 
 public class PlayerRoundIterator {
     
-    public PlayerRoundIterator() {
-        super();
+    public PlayerRoundIterator(Player[] players) {
+        this.players = players;
     }
 
     public Stream<Player> stream() {
-        return null;
+        return Arrays.stream(players);
     }
 
     public Player getCurrentPlayer() {
-        return null;
+        return players[current];
     }
+
     public Player getPlayerById(UUID id) {
+        for (Player player : players) {
+            if (player.getId().equals(id)) {
+                return player;
+            }
+        }
         return null;
     }
 
     public void reverseDirection() {
-        
+        if (direction.equals(Direction.CLOCKWISE)) {
+            direction = Direction.COUNTER_CLOCKWISE;
+        } else {
+            direction = Direction.CLOCKWISE;
+        }
     }
 
     public Player next() {
-        return null;
+        current = getNextIndex();
+        return getCurrentPlayer();
     }
 
     private int getNextIndex() {
-        return 0;
+        int increment;
+        if (direction == Direction.CLOCKWISE) {
+            increment = 1;
+        } else {
+            increment = -1;
+        }
+        return (players.length + current + increment) % players.length;
     }
+
+    private final Player[] players;
+    private int current = 0;
+    private Direction direction = Direction.CLOCKWISE;
 }
