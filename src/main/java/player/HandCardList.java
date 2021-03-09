1e1b49fca3c0f08c1a930d5daf6019bc7e07ae5e
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-03-09 20:32:37 -0300

    Implemented HandCardList

--- src/main/java/player/HandCardList.java
+++ src/main/java/player/HandCardList.java
@@ -5,41 +5,43 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import src.main.java.card.CardType;
+import src.main.java.card.CardUtil;
 import src.main.java.card.ICard;
 
 public class HandCardList {
 
-    public HandCardList() {
-        this.handCards = new ArrayList<ICard>();
-    }
-
     public void addCard(ICard card) {
-
+        handCards.add(card);
     }
 
     public boolean removeCard(ICard card) {
-
-        return true;
+        ICard cardToRemove = CardUtil.isWildCard(card) ? findCardOfType(card.getType()) : card;
+        return handCards.remove(cardToRemove);
     }
 
     private ICard findCardOfType(CardType type) {
+        for (ICard currentCard : handCards) {
+            if (currentCard.getType() == type) {
+                return currentCard;
+            }
+        }
 
         return null;
     }
 
     public Stream<ICard> getCardStream() {
-        return null;
+        return handCards.stream();
     }
 
     public boolean hasCard(ICard card) {
-
-        return true;
+        return CardUtil.isWildCard(card) ?
+                    getCardStream().anyMatch(c -> c.getType() == card.getType()) :
+                    getCardStream().anyMatch(c -> c.equals(card));
     }
 
     public int size() {
-        return 0;
-
+        return handCards.size();
     }
 
-    private final List<ICard> handCards;
+    private final List<ICard> handCards = new ArrayList<ICard>();
 }
--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,18 +1,20 @@
 package src.main.java.player;
 
+import java.util.UUID;
 import java.util.stream.Stream;
 
 import src.main.java.card.ICard;
 
-public class Player {
+public class Player extends Entity{
 
-    public Player(String name, HandCardList hand) {
+    public Player(UUID id, String name, HandCardList hand) {
+        super(id);
         this.name= name;
         this.handCards = hand;
     }
 
     public String getName() {
-        return "";
+        return name;
     }
 
     public Stream<ICard> getHandCards() {
