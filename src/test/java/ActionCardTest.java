8b5facb3b7369558c26945f1ad95b8c0d30cb1e7
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-03-13 18:41:40 -0300

    Firs test

--- src/test/java/ActionCardTest.java
+++ src/test/java/ActionCardTest.java
@@ -1,9 +1,32 @@
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import card.ActionCard;
+import card.CardColor;
+import card.CardType;
 
 public class ActionCardTest {
 
 
+    @BeforeEach
+    void setup() {
+        aCard = new ActionCard(CardType.DRAW_TWO, CardColor.YELLOW);
+    }
+
+    @Test
+    public void toStringTest() { 
+        // System.out.println(aCard);       
+        assertEquals(   "Action Card {" + 
+                        CardColor.YELLOW + ", " +
+                        CardType.DRAW_TWO + "}" ,
+                        aCard.toString());
+    }
     
+
+
+   private ActionCard aCard; 
 }
