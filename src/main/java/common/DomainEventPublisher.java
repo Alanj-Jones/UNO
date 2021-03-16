a60e21bd6031b2f9d0745df959e65007b7b79d67
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-03-16 20:08:42 -0300

    Setup DomainEventPublisher

--- src/main/java/common/DomainEventPublisher.java
+++ src/main/java/common/DomainEventPublisher.java
@@ -1,5 +1,28 @@
 package common;
 
+import java.util.List;
+
 public class DomainEventPublisher {
-    
+
+    private void subscribe(DomainEventSubscriber subscriber) {
+
+    }
+
+    private void unsubscribe(DomainEventSubscriber unsuscriber) {
+
+    }
+
+    private void publish(DomainEvent event) {
+
+    }
+
+    private void reset() {
+        
+    }
+
+
+
+    private final  ThreadLocal<List<DomainEventSubscriber>> subscribers;
+    private final ThreadLocal<Boolean> isPublishing;
+
 }
